ddebc8b3f01e1f678e8053b2fb867afc81fb4060
m7a7c7c7 <90213567+m7a7c7c7@users.noreply.github.com>
2021-12-06 00:42:17 -0500

    heroku6

--- fronted/src/APis/Facturascrud.js
+++ fronted/src/APis/Facturascrud.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:5000";
+const url = "https://easy-store-backend.herokuapp.com";
 
 export function searchFac(callback){
     axios.get(url+"/Facturas")
